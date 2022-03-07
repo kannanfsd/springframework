1b28b33f8fac2e4b328b2d95fbbf62ae91593a58
Kannan Rajendran <kannannglabs@gmail.com>
2022-03-07 23:29:24 +0530

    introduce autowired

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/55/3083c6cd3f9e001c1a5d8ad7b83fa901
@@ -0,0 +1,10 @@
+package com.nglabs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+	public void code(){
+		System.out.println("I'm coding");
+	}
+}
---
+++ boot-app/src/main/java/com/nglabs/Laptop.java
@@ -0,0 +1,12 @@
+package com.nglabs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		System.out.println("I'm compile code.");		
+	}
+
+}
