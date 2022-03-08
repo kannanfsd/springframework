a8fafcf93ae1a165c949327b97605d36c6067b8a
Kannan Rajendran <kannannglabs@gmail.com>
2022-03-08 11:41:39 +0530

    update spring-core scope

--- spring-app/src/main/java/com/nglabs/Alien.java
+++ spring-app/src/main/java/com/nglabs/Alien.java
@@ -1,6 +1,10 @@
 package com.nglabs;
 
 public class Alien {
+	int age;
+	public Alien() {
+		System.out.println("Alien Object is created.");
+	}
 	public void code() {
 		System.out.println("I'm coding.");
 	}
--- spring-app/src/main/java/com/nglabs/App.java
+++ spring-app/src/main/java/com/nglabs/App.java
@@ -3,6 +3,7 @@ package com.nglabs;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -13,10 +14,17 @@ public class App
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
     	
-        Alien obj = (Alien)factory.getBean("alien");
-        
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+    	
+        Alien obj = (Alien)factory.getBean("alien");        
         obj.code();
+        obj.age = 15;
+        System.out.println(obj.age);
+        
+        Alien obj2 = (Alien)factory.getBean("alien");        
+        obj2.code();
+        System.out.println(obj2.age);
     }
 }
